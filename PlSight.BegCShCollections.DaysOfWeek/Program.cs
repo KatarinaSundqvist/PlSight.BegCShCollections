89f8de73445624c3d6a3e827e4544bfed3e9042c
KatarinaSundqvist <singsongkat@live.com>
2020-01-08 12:14:49 +0100

    Add project files.

---
+++ PlSight.BegCShCollections.DaysOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlSight.BegCShCollections.DaysOfWeek {
+    class Program {
+        static void Main(string[] args) {
+            
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+        }
+    }
+}
